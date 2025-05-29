112e86f06abe4ef45fb9a75ce753381ab09bcbb1
mhardock <mhardock@gmail.com>
2025-05-29 08:40:46 -0400

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SettingsProvider } from "@/components/settings-provider";
 import { SidebarStateProvider } from "@/components/sidebar-state-provider";
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,6 +35,7 @@ export default function RootLayout({
             {children}
           </SidebarStateProvider>
         </SettingsProvider>
+        <Analytics />
       </body>
     </html>
   );
